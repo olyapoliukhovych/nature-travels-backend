2415b5ae60879fc5fcb666cc8d8a8e5caeefe4be
Olabi Elore <olabielore@gmail.com>
2026-03-28 22:08:07 +0200

    feat: Adding a logout user endpoint

---
+++ src/controllers/authController.js
@@ -0,0 +1,15 @@
+import { Session } from '../models/session.js';
+
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
+};

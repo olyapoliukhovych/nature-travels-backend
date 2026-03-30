350726145fe1248c572ed24422d0c363b9602c33
Olesia Tsaturian <olesiases@gmail.com>
2026-03-30 17:52:00 +0300

    add /users enpoints

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,11 +4,25 @@ import {
   updateUserAvatar,
   updateUser,
   verifyUserEmail,
+  deleteStory,
+  getCurrentUser,
+  getMyStories,
+  getSavedStories,
+  getUserById,
+  saveStory,
 } from '../controllers/userController.js';
 import { upload } from '../middleware/multer.js';
 import { updateUserSchema } from '../validation/updateUserValidation.js';
 
 const router = Router();
+
+router.get('/users/:userId', getUserById);
+
+router.get('/users/me', authenticate, getCurrentUser);
+router.post('/users/me/saved/:storyId', authenticate, saveStory);
+router.delete('/users/me/saved/:storyId', authenticate, deleteStory);
+router.get('/users/me/stories', authenticate, getMyStories);
+router.get('/users/me/saved', authenticate, getSavedStories);
 
 router.patch(
   '/users/me/avatar',

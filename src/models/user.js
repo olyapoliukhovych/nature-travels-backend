33b135a6e498de3baa7ff3762b7a082c5af630dd
Olesia Tsaturian <olesiases@gmail.com>
2026-03-30 17:39:37 +0300

    add endpoints for users

--- src/models/user.js
+++ src/models/user.js
@@ -3,20 +3,15 @@ import { COLLECTIONS } from '../constants/collections.js';
 
 const userSchema = new Schema(
   {
-    name: { type: String, trim: true },
+    name: { type: String, required: true, trim: true },
     email: { type: String, unique: true, required: true, trim: true },
     password: { type: String, required: true },
-    articlesAmount: { type: Number, default: 0 },
-    savedStories: [{ type: Schema.Types.ObjectId, ref: COLLECTIONS.ARTICLE }],
+    avatarUrl: { type: String, default: null },
+    emailVerified: { type: Boolean, default: false },
+    storiesAmount: { type: Number, default: 0 },
+    savedStories: [{ type: Schema.Types.ObjectId, ref: COLLECTIONS.STORY }],
   },
   { timestamps: true, versionKey: false },
 );
-
-userSchema.pre('save', function (next) {
-  if (!this.username) {
-    this.username = this.email;
-  }
-  next();
-});
 
 export const User = model(COLLECTIONS.USER, userSchema);

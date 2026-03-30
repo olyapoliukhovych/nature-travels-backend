9af0e45748fe4448958b72873bee6a2c1ae33deb
Ivan <ivanolef.io@gmail.com>
2026-03-30 12:15:19 +0300

    fixed validation

--- src/validation/authValidation.js
+++ src/validation/authValidation.js
@@ -1,16 +1,16 @@
-import { Joi, Segments, celebrate } from 'celebrate';
+import { Joi, Segments } from 'celebrate';
 
-export const registerSchema = celebrate({
+export const registerSchema = {
   [Segments.BODY]: Joi.object().keys({
     username: Joi.string().min(3).max(32).required(),
     email: Joi.string().email().max(64).required(),
     password: Joi.string().min(8).max(128).required(),
   }),
-});
+};
 
-export const loginSchema = celebrate({
+export const loginSchema = {
   [Segments.BODY]: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
-});
+};

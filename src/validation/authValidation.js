47e05df257cca6f9032c9baf35c2776fe4129336
Maksym Khlysta <maksimhlista1337@gmail.com>
2026-03-31 15:46:38 +0300

    fix(auth): update registration validation logic to prevent submission errors

--- src/validation/authValidation.js
+++ src/validation/authValidation.js
@@ -2,7 +2,7 @@ import { Joi, Segments } from 'celebrate';
 
 export const registerSchema = {
   [Segments.BODY]: Joi.object().keys({
-    username: Joi.string().min(3).max(32).required(),
+    name: Joi.string().min(3).max(32).required(),
     email: Joi.string().email().max(64).required(),
     password: Joi.string().min(8).max(128).required(),
   }),

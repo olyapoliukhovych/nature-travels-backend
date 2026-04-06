44d34a62cc46985e673611eb527f48115512aab8
Olya Poliukhovych <olha.poliukhovych.1@gmail.com>
2026-04-06 17:19:50 +0300

    fix: update cors

---
+++ src/config/corsOptions.js
@@ -0,0 +1,21 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://nature-travels-frontend.vercel.app',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+export default corsOptions;

24a6dd0b2d370c0c236f9d36da75c98d852fd40e
Olya Poliukhovych <olha.poliukhovych.1@gmail.com>
2026-05-06 14:42:51 +0300

    test: update cors and sendMail

--- src/server.js
+++ src/server.js
@@ -18,7 +18,12 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_DOMAIN, 'http://localhost:3000'],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 //!ROUTES
